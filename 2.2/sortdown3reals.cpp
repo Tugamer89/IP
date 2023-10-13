0bc3e07a5238ed9ae08b56050fb91582787e3a1d
Tugamer89 <61603718+Tugamer89@users.noreply.github.com>
2023-10-13 10:46:33 +0200

    Improved performaces

--- 2.2/sortdown3reals.cpp
+++ 2.2/sortdown3reals.cpp
@@ -7,16 +7,26 @@ int main() {
     cout << "Inserire tre numeri reali: ";
     cin >> a >> b >> c;
 
-    float nMax = max(max(a, b), c);
-    float nMin = min(min(a, b), c);
-    float nMid = a;
+    cout << "Numeri ordinati: ";
 
-    if (b != nMax && b != nMin)
-        nMid = b;
-    else if (c != nMax && c != nMin)
-        nMid = c;
-
-    cout << "Numeri ordinati: " << nMax << " " << nMid << " " << nMin << endl;
+    if (a >= b && a >= c) {
+        if (b >= c)
+            cout << a << " " << b << " " << c << endl;
+        else
+            cout << a << " " << c << " " << b << endl;
+    }
+    else if (b >= a && b >= c) {
+        if (a >= c)
+            cout << b << " " << a << " " << c << endl;
+        else
+            cout << b << " " << c << " " << a << endl;
+    }
+    else {
+        if (b >= a)
+            cout << c << " " << b << " " << a << endl;
+        else
+            cout << c << " " << a << " " << b << endl;
+    }
 
     return 0;
 }
