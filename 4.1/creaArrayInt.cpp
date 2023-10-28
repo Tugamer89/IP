d4f46286ed2417a6236c5c2c6934bf8813ccfe26
Tuga <61603718+Tugamer89@users.noreply.github.com>
2023-10-28 11:24:44 +0200

    Fixed programs

--- 4.1/creaArrayInt.cpp
+++ 4.1/creaArrayInt.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
-#define N 10
 
 using namespace std;
 
 int main() {
+    const int N = 10;
     int a[N];
 
     for (int i = 0; i < N; ++i)
         a[i] = N-i;
 
     for (int i = 0; i < N; ++i)
-        cout << a[i] << endl;
+        cout << "Valore di a[" << i << "] = " <<  a[i] << endl;
 
     return 0;
 }
--- 4.1/stampaArrayFloat.cpp
+++ 4.1/stampaArrayFloat.cpp
@@ -6,7 +6,7 @@ int main() {
     float a[5] = {2.4, 5.67, 34, 28.456, 846.42};
 
     for (int i = 4; i >= 0; --i)
-        cout << a[i] << endl;
+        cout << "Valore di a[" << i << "] = " <<  a[i] << endl;
 
     return 0;
 }
