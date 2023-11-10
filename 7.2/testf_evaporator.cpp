28080a96624791ff994b507dfb2dcca7e9bfab39
Tugamer89 <61603718+Tugamer89@users.noreply.github.com>
2023-11-10 10:48:10 +0100

    Improved performance

--- 7.2/testf_evaporator.cpp
+++ 7.2/testf_evaporator.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
 
+using namespace std;
+
 int remainingDays(float initialLiters, int dailyEvapPercentage, float minLiters) {
+    // dynamic programming
+    /*
     int days = 0;
     float currentLiters = initialLiters;
     
-    while (currentLiters > minLiters) {
+    while (currentLiters >= minLiters) {
         currentLiters -= initialLiters * dailyEvapPercentage / 100;
         ++days;
     }
     
     return days;
+    */
+
+    // static programming
+    return (100 - (minLiters/initialLiters * 100)) / dailyEvapPercentage + 1;
+}
+
+int main() {
+    float initialLiters, minLiters;
+    int dailyEvapPercentage;
+
+    cout << "Inserire litri iniziali: ";
+    cin >> initialLiters;
+
+    cout << "Inserire litri minimi: ";
+    cin >> minLiters;
+
+    cout << "Inserire percentuale di evaporazione giornaliera: ";
+    cin >> dailyEvapPercentage;
+
+    cout << "L'evaporatore rimane attivo per " << remainingDays(initialLiters, dailyEvapPercentage, minLiters) << " giorni" << endl;
+
+    return 0;
 }
