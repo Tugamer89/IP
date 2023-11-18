43b0df1aa1fe1385732cb9de793630177262553a
Tuga <61603718+Tugamer89@users.noreply.github.com>
2023-11-18 12:38:51 +0100

    Improved programs

--- 8.2/testf_population_sim.cpp
+++ 8.2/testf_population_sim.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
@@ -7,22 +8,30 @@ struct OutOfRangeError {
     string functionName;
     string paramName;
     string paramValue;
-    OutOfRangeError(string fName, string pName, string pValue) : functionName(fName), paramName(pName), paramValue(pValue) {}
+    string customError;
+    OutOfRangeError(string fName, string pName, string pValue, string cError) : functionName(fName), paramName(pName), paramValue(pValue), customError(cError) {}
 };
 
 int populationAfterYears(int initPop, int pBorn, int nMoved, int years = 1) {
     if (initPop < 0)
-        throw OutOfRangeError("populationAfterYears", "initPop", to_string(initPop));
+        throw OutOfRangeError("populationAfterYears", "initPop", to_string(initPop), "la popolazione iniziale è negativa");
     
     if (abs(pBorn) > 100)
-        throw OutOfRangeError("populationAfterYears", "pBorn", to_string(pBorn));
+        throw OutOfRangeError("populationAfterYears", "pBorn", to_string(pBorn), "la percentuale di nascite al netto delle morti non è valida");
 
+    if (years < 0)
+        throw OutOfRangeError("populationAfterYears", "years", to_string(years), "gli anni sono negativi");
+
+    /*
     int pop = initPop;
     for (int i = 0; i < years; ++i)
         pop += pop * pBorn / 100.0 + nMoved;
+    */
+
+    int pop = initPop * pow(1 + pBorn/100.0, years) + nMoved*years;
 
     if (pop < 0)
-        throw (string)"la popolazione finale è negativa";
+        throw OutOfRangeError("populationAfterYears", "nMoved", to_string(nMoved), "si trasferiscono più persone di quante ce ne sono");
 
     return pop;
 }
@@ -44,10 +53,8 @@ int main() {
         cout << "La popolazione dopo un anno è " << lastPop << endl;
     }
     catch (OutOfRangeError& err) {
-        cerr << "ERRORE: durante la chiamata di " << err.functionName << " con " << err.paramName << " = " << err.paramValue << endl;
-    }
-    catch (string& err) {
-        cerr << "ERRORE: " << err << "!" << endl;
+        cerr << "\e[31mERRORE: durante la chiamata di " << err.functionName << " con " << err.paramName << " = " << err.paramValue << 
+            ", messaggio d'errore: " << err.customError << "!\e[0m" << endl;
     }
 
     return 0;
