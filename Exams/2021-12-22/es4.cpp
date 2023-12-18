e8332357533cd34328472df02e74d7e517d21777
Tuga <61603718+Tugamer89@users.noreply.github.com>
2023-12-18 12:49:48 +0100

    Improved code

--- Exams/2021-12-22/es4.cpp
+++ Exams/2021-12-22/es4.cpp
@@ -14,19 +14,29 @@ struct array_dinamico {
     un puntatore ad un array e non un array
 */
 
-array_dinamico init_ad() {
+array_dinamico init_ad(unsigned int size = 0) {
     array_dinamico tmp;
-    tmp.d = nullptr;
-    tmp.size = 0;
-    tmp.capacity = 0;
+    if (size == 0)
+        tmp.d = nullptr;
+    else
+        tmp.d = new int[size];
+    tmp.size = size;
+    tmp.capacity = size;
     return tmp;
 }
 
 int main() {
     array_dinamico a = init_ad();
+    cout << "Dynamic array a:" << endl;
     cout << "d = " << static_cast<void*>(a.d) << endl;
     cout << "size = " << a.size << endl;
     cout << "capacity = " << a.capacity << endl;
+
+    array_dinamico b = init_ad(5);
+    cout << "\nDynamic array b:" << endl;
+    cout << "d = " << static_cast<void*>(b.d) << endl;
+    cout << "size = " << b.size << endl;
+    cout << "capacity = " << b.capacity << endl;
 
     return 0;
 }
