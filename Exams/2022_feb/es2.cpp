fb3ffdbe6361cd90974d1ced0c9c9090b5d7f682
Tuga <61603718+Tugamer89@users.noreply.github.com>
2023-12-18 17:23:32 +0100

    Fixed code

--- Exams/2022_feb/es2.cpp
+++ Exams/2022_feb/es2.cpp
@@ -3,19 +3,18 @@
 using namespace std;
 
 int CountOccurrences(int A[], int size, int val) {
-    int occs = 0;
+    if (size == 0)
+        return 0;
 
-    for (int i = 0; i < size; ++i)
-        if (A[i] == val)
-            ++occs;
-
-    return occs;
+    return (A[size-1] == val) + CountOccurrences(A, size-1, val);
 }
 
 int main() {
     int a[] = {1, 23, 52, 1, 1, 9, -9, 0, 1};
 
     cout << "Le occorrenze di 1 sono " << CountOccurrences(a, 9, 1) << endl;
+    cout << "Le occorrenze di 18 sono " << CountOccurrences(a, 9, 18) << endl;
+    cout << "Le occorrenze di -9 sono " << CountOccurrences(a, 9, -9) << endl;
 
     return 0;
 }
