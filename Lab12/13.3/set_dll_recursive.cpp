b661e30e506f0c6c784eb211a58dfa350efcbea3
Tuga <61603718+Tugamer89@users.noreply.github.com>
2024-01-08 20:26:13 +0100

    Improved code

--- Lab12/13.3/set_dll_recursive.cpp
+++ Lab12/13.3/set_dll_recursive.cpp
@@ -3,24 +3,24 @@
 
 using namespace std;
 
-void insert_set_dll(set_dll &s, int v) {
+void insert_set_dll(set_dll& s, int v) {
     // TODO
 }
 
-void print_set_dll(const set_dll &s) {
+void print_set_dll(const set_dll& s) {
     // TODO
 }
 
-void revprint_set_dll(const set_dll &s) {
+void revprint_set_dll(const set_dll& s) {
     // TODO
 }
 
-set_dll union_set_dll(const set_dll &s1, const set_dll &s2) {
+set_dll union_set_dll(const set_dll& s1, const set_dll& s2) {
     // TODO
     return nullptr;
 }
 
-set_dll intersect_set_dll(const set_dll &s1, const set_dll &s2) {
+set_dll intersect_set_dll(const set_dll& s1, const set_dll& s2) {
     // TODO
     return nullptr;
 }
