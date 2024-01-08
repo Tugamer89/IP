79674c339db9abbf6639b55017aacc0eee4a55f0
Tuga <61603718+Tugamer89@users.noreply.github.com>
2024-01-08 20:12:07 +0100

    Fixed code

--- Lab12/13.3/set_dll_recursive.cpp
+++ Lab12/13.3/set_dll_recursive.cpp
@@ -4,32 +4,23 @@
 using namespace std;
 
 void insert_set_dll(set_dll &s, int v) {
-
+    // TODO
 }
-// inserisce un nuovo elemento (con data = v) nel set s
 
 void print_set_dll(const set_dll &s) {
-
+    // TODO
 }
-// stampa il contenuto set s dal primo all'ultimo elemento
-// (in linea con elementi separati da "->")
 
 void revprint_set_dll(const set_dll &s) {
-
+    // TODO
 }
-// stampa il contenuto set s dall'ultimo al primo elemento
-// (in linea e con elementi separati da "->")
 
 set_dll union_set_dll(const set_dll &s1, const set_dll &s2) {
-
+    // TODO
+    return nullptr;
 }
-// ritorna un set che rappresenta l'unione dei set s1 e s2
-// NB: ogni elemento del nuovo set deve essere un duplicato di quelli contenuti in s1 e s2
-// in questo modo sia s1 che s2 non subiscono alcuna modifica
 
 set_dll intersect_set_dll(const set_dll &s1, const set_dll &s2) {
-    
+    // TODO
+    return nullptr;
 }
-// ritorna un set che rappresenta l'intersezione dei set s1 e s2
-// NB: ogni elemento del nuovo set deve essere un duplicato di quelli contenuti in s1 e s2
-// in questo modo sia s1 che s2 non subiscono alcuna modifica
